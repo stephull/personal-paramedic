869823f3747336c8c9a8b22ca903221533edebd4
Matthew O'Mara <tuh39725@temple.edu>
2022-12-09 00:59:26 -0500

    add graphview library to project

--- app/src/main/java/edu/temple/falldetection/MainActivity.kt
+++ app/src/main/java/edu/temple/falldetection/MainActivity.kt
@@ -20,10 +20,33 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
 
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also {
+            sensorManager.registerListener(
+                this,
+                it,
+                SensorManager.SENSOR_DELAY_FASTEST,
+                SensorManager.SENSOR_DELAY_FASTEST
+            )
+        }
+
     }
 
-    override fun onSensorChanged(p0: SensorEvent?) {
-        TODO("Not yet implemented")
+    override fun onSensorChanged(event: SensorEvent?) {
+        val alpha: Float = 0.8f
+        val linearAcceleration = floatArrayOf(.1f)
+        if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER){
+
+            // Isolate the force of gravity with the low-pass filter.
+            event.values[0] = alpha * event.values[0] + (1 - alpha) * event.values[0]
+            event.values[1] = alpha * event.values[1] + (1 - alpha) * event.values[1]
+            event.values[2] = alpha * event.values[2] + (1 - alpha) * event.values[2]
+
+            // Remove the gravity contribution with the high-pass filter.
+            linearAcceleration[0] = event.values[0] - event.values[0]
+            linearAcceleration[1] = event.values[1] - event.values[1]
+            linearAcceleration[2] = event.values[2] - event.values[2]
+
+        }
     }
 
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
