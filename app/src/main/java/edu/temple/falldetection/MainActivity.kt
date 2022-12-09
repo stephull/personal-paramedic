ee35bb662d88b82ecff8f00b4add68344df7ed2a
Matthew O'Mara <tuh39725@temple.edu>
2022-12-09 01:51:07 -0500

    add text views to show sensor data

--- app/src/main/java/edu/temple/falldetection/MainActivity.kt
+++ app/src/main/java/edu/temple/falldetection/MainActivity.kt
@@ -7,15 +7,23 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private lateinit var sensorManager: SensorManager
     private lateinit var sensor: Sensor
+    private lateinit var x_cordinate: TextView
+    private lateinit var y_cordinate: TextView
+    private lateinit var z_cordinate: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        x_cordinate = findViewById(R.id.x)
+        y_cordinate = findViewById(R.id.y)
+        z_cordinate = findViewById(R.id.z)
 
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
@@ -33,14 +41,21 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     override fun onSensorChanged(event: SensorEvent?) {
         val alpha: Float = 0.8f
-        val linearAcceleration = floatArrayOf(.1f)
+        val x: Float
+        val y: Float
+        val z: Float
+//        val linearAcceleration = floatArrayOf(.1f)
+
         if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER){
 
             // Isolate the force of gravity with the low-pass filter.
-            event.values[0] = alpha * event.values[0] + (1 - alpha) * event.values[0]
-            event.values[1] = alpha * event.values[1] + (1 - alpha) * event.values[1]
-            event.values[2] = alpha * event.values[2] + (1 - alpha) * event.values[2]
+            x = alpha * event.values[0] + (1 - alpha) * event.values[0]
+            y = alpha * event.values[1] + (1 - alpha) * event.values[1]
+            z = alpha * event.values[2] + (1 - alpha) * event.values[2]
 
+            x_cordinate.text = x.toString()
+            y_cordinate.text = y.toString()
+            z_cordinate.text = z.toString()
             // Remove the gravity contribution with the high-pass filter.
 //            linearAcceleration[0] = event.values[0] - event.values[0]
 //            linearAcceleration[1] = event.values[1] - event.values[1]
@@ -50,7 +65,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     }
 
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-        TODO("Not yet implemented")
+        return
     }
 
     override fun onDestroy() {
